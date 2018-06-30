58a73957f8db8beabee800cf72c612ceb8175160
SlawekPelka <s.pelka@live.nl>
2018-06-30 18:49:52 +0200

    :pencil: if you now mention greet/goodbye bot, he replies to you

--- modules/goodbye.js
+++ modules/goodbye.js
@@ -2,16 +2,23 @@ const util = require('../brain/utilitychecker');
 
 let cmd = {
     exec: (message, args) => {
-        let greetings = ['goodbye', 'be gone', 'cya', 'farewell', 'bye noob', 'see ya later'];
+        let farewells = ['goodbye', 'be gone', 'cya', 'farewell', 'bye noob', 'see ya later'];
 
         if (message.mentions.users.size != 0) {
             let mentionedUser = message.mentions.users.first();
-            let guildUser = message.guild.members.get(mentionedUser.id);
-            let mentionedPersonName = guildUser.nickname || guildUser.displayName;
-            let authorName = message.author.username;
-            message.channel.send(`${authorName} says **${util.trueRandom(greetings)}** to ${mentionedPersonName}`);
+            let mentionedGuildUser = message.guild.members.get(mentionedUser.id);
+            let mentionedPersonName = mentionedGuildUser.nickname || mentionedGuildUser.displayName;
+
+            let guildAuthor = message.guild.members.get(message.author.id)
+            let authorName = guildAuthor.nickname || guildAuthor.displayName;
+
+            if (mentionedGuildUser.id == 455827368586772507 || mentionedGuildUser.id == 459451947506728961) {
+                message.channel.send(`${util.trueRandom(farewells)} ${authorName}`);
+            } else {
+                message.channel.send(`${authorName} says **${util.trueRandom(farewells)}** to ${mentionedPersonName}`);
+            }
         } else {
-            message.channel.send(util.trueRandom(greetings));
+            message.channel.send(util.trueRandom(farewells));
             return;
         }
 
--- modules/hello.js
+++ modules/hello.js
@@ -2,14 +2,21 @@ const util = require('../brain/utilitychecker');
 
 let cmd = {
     exec: (message, args) => {
-        let greetings = ['hello', 'hi', 'aloha', 'g\'day', 'tomat shall return'];
+        let greetings = ['hello', 'hi', 'aloha', 'g\'day', 'heyo'];
 
         if (message.mentions.users.size != 0) {
             let mentionedUser = message.mentions.users.first();
-            let guildUser = message.guild.members.get(mentionedUser.id);
-            let mentionedPersonName = guildUser.nickname || guildUser.displayName;
-            let authorName = message.author.username;
-            message.channel.send(`${authorName} says **${util.trueRandom(greetings)}** to ${mentionedPersonName}`);
+            let mentionedGuildUser = message.guild.members.get(mentionedUser.id);
+            let mentionedPersonName = mentionedGuildUser.nickname || mentionedGuildUser.displayName;
+
+            let guildAuthor = message.guild.members.get(message.author.id)
+            let authorName = guildAuthor.nickname || guildAuthor.displayName;
+
+            if (mentionedGuildUser.id == 455827368586772507 || mentionedGuildUser.id == 459451947506728961) {
+                message.channel.send(`${util.trueRandom(greetings)} ${authorName}`);
+            } else {
+                message.channel.send(`${authorName} says **${util.trueRandom(greetings)}** to ${mentionedPersonName}`);
+            }
         } else {
             message.channel.send(util.trueRandom(greetings));
             return;
